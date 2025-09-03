1c4aff673c1a59d9fcf2a10d68f122473f932797
SantiBetancur <santyagobetancurth2004@gmail.com>
2025-09-02 23:01:57 -0500

    Tutorial01 - Completed

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,86 @@
 from django.shortcuts import render 
-from django.http import HttpResponse 
+from django.views.generic import TemplateView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
     pass
+class AboutPageView(TemplateView):
+ template_name = 'pages/about.html'
+
+ def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context.update({
+    "title": "About us - Online Store",
+    "subtitle": "About us",
+    "description": "This is an about page ...",
+    "author": "Developed by: Santiago",
+    })
+    return context
+
+class Product:
+    products = [
+ {"id":"1", "name":"TV", "description":"Best TV", "price":800},
+ {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":1000},
+ {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":30},
+ {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":100}
+ ]
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+        viewData["products"] = Product.products
+        return render(request, self.template_name, viewData)
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+    def get(self, request, id):
+        viewData = {}
+        try:
+            product = Product.products[int(id)-1]
+            viewData["title"] = product["name"] + " - Online Store"
+            viewData["subtitle"] = product["name"] + " - Product information"
+            viewData["product"] = product
+            viewData["price"] = product["price"]
+            return render(request, self.template_name, viewData)
+        except (ValueError, IndexError):
+            return HttpResponseRedirect(reverse('home'))
+
+
+from django import forms
+from django.shortcuts import render, redirect
+
+class ProductForm(forms.Form):
+
+    name = forms.CharField(required=True)
+    price = forms.FloatField(required=True)
+    
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price <= 0:
+            raise forms.ValidationError("Price must be greater than zero.")
+        return price
+
+class ProductCreateView(View):
+    template_name = 'products/create.html'
+    def get(self, request):
+        form = ProductForm()
+        viewData = {}
+        viewData["title"] = "Create product"
+        viewData["form"] = form
+        return render(request, self.template_name, viewData)
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            return render(request, 'products/success.html', {"title": "Product Created"})
+        else:
+            viewData = {}
+            viewData["title"] = "Create product"
+            viewData["form"] = form
+            return render(request, self.template_name, viewData)
